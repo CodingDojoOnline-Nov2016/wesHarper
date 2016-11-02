b2205f817e796c12e20e89af6ac83374f5c49670
wesviewzr <wes@viewzr.com>
2016-11-02 15:25:06 -0600

    added algorithms

---
+++ Algorithms/countingTheDojoWay.js
@@ -0,0 +1,26 @@
+//Page 16
+//Print integers 1 to 100. If divisible by 5, print "Coding" instead. If by 10, also print " Dojo".
+
+function dojoWay(){
+	for(var num = 1; num < 101; num++) {
+		if(num % 5 === 0 && num % 10 === 0) {
+			console.log("Coding Dojo");
+		} else if(num % 5 === 0) {
+			console.log("Coding");
+		} else {
+			console.log(num);
+		}
+	}
+}
+
+dojoWay();
+
+//shoot for solving problem in 1 line
+function dojoWay2(){
+	for(var num = 1; num < 101; num++) {
+		value = num % 5 === 0 && num % 10 === 0 ? "Coding Dojo" : value = num % 5 === 0 ? "Coding" : num;
+		console.log(value);
+	}
+}
+
+dojoWay2();
---
+++ Algorithms/whatDoYouKnow.js
@@ -0,0 +1,8 @@
+//Page 16
+//Your function will be given an input parameter incoming. Please console.log this value.
+
+function whatIKnow(incoming) {
+	console.log(incoming);
+}
+
+whatIKnow("Something");
