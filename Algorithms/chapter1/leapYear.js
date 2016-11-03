ce26246d90dea008e5f7824c93bd9aa83cdd4113
wesviewzr <wes@viewzr.com>
2016-11-03 11:02:36 -0600

    finished algorithms page 16 and created freeBSD folders

---
+++ Algorithms/chapter1/leapYear.js
@@ -0,0 +1,18 @@
+//Page 16
+//Write a function that determines whether a given year is a leap year.
+//If a year is divisible by four, it is a leap year, unless it is divisible by 100.
+//However, if it is divisible by 400 then it is.
+
+function isLeapYear(year) {
+	if((Math.abs(year % 4) === 0 && Math.abs(year % 100) != 0) || Math.abs(year % 400) === 0) {
+		console.log("Yep, that's a leap year.")
+	} else {
+		console.log("Nah, that's not a leap year.")
+	}
+}
+
+isLeapYear(2000);
+isLeapYear(1991);
+isLeapYear(400);
+isLeapYear(500);
+isLeapYear(-400);
