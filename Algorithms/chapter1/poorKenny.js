54fa473958a87fad8c989d1842e1108a9e9fc319
wesviewzr <wes@viewzr.com>
2016-11-06 09:55:19 -0700

    started PPCLubricants

---
+++ Algorithms/chapter1/poorKenny.js
@@ -0,0 +1,9 @@
+// Page 24
+// Kenny Tries to stay safe, but somehow every day something happens.
+// If there is a 10% chance of volcano, 15% chance of tsunami, 20% chance of earthquake,
+// 25% chance of blizzard, and 30% chance of meteor strike, 
+// write function whatHappensToday() to print the outcome.
+
+function whatHappensToday() {
+
+}
