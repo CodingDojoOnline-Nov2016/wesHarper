d8eca77d85c1a4cb788aae8747e6981708f94d9f
wesviewzr <wes@viewzr.com>
2016-12-21 15:23:24 -0700

    finished deques algos

--- python_stack/flask_mySQL/full_friends/server.py
+++ python_stack/flask_mySQL/full_friends/server.py
@@ -12,7 +12,19 @@ mysql = MySQLConnector(app, 'full_friends')
 def index():
 	query = 'SELECT first_name, last_name, email FROM friends'
 	friends = mysql.query_db(query)
-	print friends
 	return render_template('index.html', title="Friends Home", friends=friends)
+
+@app.route('/friends', methods=['POST'])
+def create():
+	print request.form
+	query = '''INSERT INTO friends(first_name, last_name, email, created_at, updated_at)
+	VALUES (:first_name, :last_name, :email, NOW(), NOW())'''
+	data = {
+		'first_name': request.form['first-name'],
+		'last_name': request.form['last-name'],
+		'email': request.form['email'],
+	}
+	mysql.query_db(query, data)
+	return redirect('/') 
 
 app.run(debug=True)
