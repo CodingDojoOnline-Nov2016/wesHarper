3527b994443d24d0bf9c3e58d34cc744170208ec
wesviewzr <wes@viewzr.com>
2016-12-22 19:03:13 -0700

    finished login_and_registration

--- python_stack/flask_mySQL/login_and_registration/server.py
+++ python_stack/flask_mySQL/login_and_registration/server.py
@@ -13,13 +13,12 @@ EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9\.\+_-]+@[a-zA-Z0-9\._-]+\.[a-zA-Z]*$')
 def index():
 	return render_template('index.html', title="Login and Registration")
 
-@app.route('/success/<id>/profile')
-def success(id):
+@app.route('/users/<id>/profile')
+def users(id):
 	query = """SELECT first_name, last_name FROM users
 		WHERE id = :id"""
 	data = { 'id': id }
 	user = mysql.query_db(query, data)
-	print user
 	return render_template('success.html', title="Success!", user=user)
 
 @app.route('/users', methods=['POST'])
@@ -31,6 +30,14 @@ def create():
 	confirm = request.form['confirm']
 	errors = []
 
+	#check to see if email address exists in system
+	validation_query = "SELECT id FROM users WHERE email= :email"
+	validation_data = { 'email': email }
+	user_check = mysql.query_db(validation_query, validation_data)
+	if user_check:
+		errors.append("A user exists with that email, please login or register with a different account")
+
+	#validation checks
 	if len(first) < 2:
 		errors.append("First name must be at least 2 characters long")
 	elif not first.isalpha():
@@ -49,11 +56,13 @@ def create():
 	elif password != confirm:
 		errors.append("Password and confirmation must match")
 
+	#handle errors
 	if errors:
 		for error in errors:
 			flash(error)
 		return redirect('/')
 	else:
+		#store new user in db
 		pw_hash = bcrypt.generate_password_hash(password)
 		insert_query = '''INSERT INTO users(first_name, last_name, email, pw_hash, created_at, updated_at)
 			VALUES (:first_name, :last_name, :email, :pw_hash, NOW(), NOW())'''
@@ -64,12 +73,14 @@ def create():
 			'pw_hash': pw_hash,
 		}
 		mysql.query_db(insert_query, insert_data)
+
+		#pull user_id for routing
 		user_query = "SELECT id FROM users WHERE email = :email LIMIT 1"
 		user_data = { 'email': email }
 		user = mysql.query_db(user_query, user_data)
 		flash("successful registration")
 		session['user_id'] = user[0]['id']
-		return redirect('/success/'+str(session['user_id'])+'/profile')
+		return redirect('/users/'+str(session['user_id'])+'/profile')
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -78,13 +89,28 @@ def login():
 	if email and password:
 		user_query = "SELECT id, email, pw_hash FROM users WHERE email = :email LIMIT 1"
 		query_data = { 'email': email }
-		user = mysql.query_db(user_query, query_data)
-		if bcrypt.check_password_hash(user[0]['pw_hash'], password):
-			session['user_id'] = user[0]['id']
-			flash('successful login')
-			return redirect('/success/'+str(session['user_id'])+'/profile')
+		try:
+			user = mysql.query_db(user_query, query_data)
+			if bcrypt.check_password_hash(user[0]['pw_hash'], password):
+				session['user_id'] = user[0]['id']
+				flash('successful login')
+				return redirect('/users/'+str(session['user_id'])+'/profile')
+			else:
+				flash('incorrect password')
+				return redirect('/')
+		except:
+			flash('no user exists with that email')
+			return redirect('/')
 	else:
 		flash('unsuccessful login')
 		return redirect('/')
+
+@app.route('/users/<id>/delete', methods=['POST'])
+def destroy(id):
+	query = "DELETE FROM users WHERE id= :id"
+	data = { 'id': id }
+	mysql.query_db(query, data)
+	session['user_id'] = None
+	return redirect('/')
 
 app.run(debug=True)
