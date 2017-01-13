e4befb90792bb9a569a6d219641397d6190a0e44
wesviewzr <wes@viewzr.com>
2017-01-13 12:07:10 -0700

    finished ninja_gold django

---
+++ python_stack/django/ninja_gold/apps/gold/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, redirect
+import random, datetime
+
+# Create your views here.
+def index(request):
+	if 'activities' not in request.session:
+		request.session['activities'] = []
+	if 'gold_count' not in request.session:
+		request.session['gold_count'] = 0
+	return render(request, 'gold/index.html')
+
+def process(request, location):
+	if request.method == "POST":
+		if 'gold_count' not in request.session:
+			request.session['gold_count'] = 0
+		if 'activities' not in request.session:
+			request.session['activities'] = []
+
+		locations = {
+			"farm": ("won", random.randint(10, 20)),
+			"cave": ("won", random.randint(5, 10)),
+			"house": ("won", random.randint(2, 5)),
+			"casino": ["", random.randint(-50, 50)],
+		}
+
+		for key, value in locations.items():
+			if location == key:
+				print value[1]
+				request.session['gold_count'] += value[1]
+				if location == "casino":
+					locations['casino'][0] = "won" if locations['casino'][1] >= 0 else "lost"
+					if locations['casino'][0] == "won":
+						exclam = "Nice"
+						outcome = 1
+					else:
+						exclam = "Ouch"
+						outcome = 0
+					casino_str = "Entered the casino and {} {} golds... {}... {}".format(locations['casino'][0], locations['casino'][1], exclam, datetime.datetime.now())
+					print casino_str
+					print locations['casino'][0]
+					request.session['activities'].insert(0, (outcome, casino_str))
+				else:
+					outcome = 1
+					string = "Earned {} golds from the {}! {}".format(value[1], location, datetime.datetime.now())
+					print string
+					request.session['activities'].insert(0, (outcome, string))
+					print request.session['activities']
+
+		return redirect('/')
+	else:
+		return redirect('/')
